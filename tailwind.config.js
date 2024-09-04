3808ce284e70b181928d8cc1426be365a210b181
parkseyoung <hellosy76@naver.com>
2024-09-04 22:41:31 +0900

    style: 색상선정, 기본 component, container 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#3D3D3D',
+      white: '#FFFFFF',
+      red: '#FF6666',
+      blue: '#4E46DD',
+      skyblue: '#C9D4FC',
+      gray: {
+        100: '#F3F4F6',
+        200: '#D9D9DE',
+        300: '#747483',
+      },
+    },
     extend: {},
   },
   plugins: [],
