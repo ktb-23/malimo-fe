ce2ba0fc17c9b6746b21aed1ebf7d7afa51200ef
timepresent95 <timepresent95@gmail.com>
2024-09-07 03:38:52 +0900

    fix: mistypo

--- src/constant/storageKey.js
+++ src/constant/storageKey.js
@@ -1,4 +1,4 @@
 export const ACCESS_TOKEN_KEY = 'accesstoken';
 export const REFRESH_TOKEN_KEY = 'refreshtoken';
 export const USER_ID_KEY = 'user_id';
-export const NICKNAME_KEY = 'user_id';
+export const NICKNAME_KEY = 'nickname';
